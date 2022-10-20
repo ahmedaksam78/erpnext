afe477614a68fe0a7fda8fa6fd75b33d8f9fd46e
ahmedaksam78 <ahmed787878787899@gmail.com>
2022-10-20 12:45:21 +0200

    first commit

--- erpnext/api.py
+++ erpnext/api.py
@@ -2,12 +2,12 @@ import frappe
 from frappe import _
 
 @frappe.whitelist()
-def get_sales_invoice(name):
+def get_sales_invoice(name) :
  
     try :
         doc = frappe.get_doc("Sales Invoice",name) 
         # doc = frappe.db.get_last_doc("Sales Invoice", filters={"name": name}, order_by="name desc")
-    except Exception as e  :
+    except Exception as e :
         doc = None
         frappe.throw(_("Doctype Not Found")) 
     return doc
