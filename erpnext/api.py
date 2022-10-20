53dc5de91dd0b73b88cd636bf58fc01a3634b7c8
ahmedaksam78 <ahmed787878787899@gmail.com>
2022-10-20 12:19:34 +0200

    init

--- erpnext/api.py
+++ erpnext/api.py
@@ -5,9 +5,9 @@ from frappe import _
 def get_sales_invoice(name):
  
     try:
-        doc = frappe.db.get_doc("Sales Invoice",name) 
+        doc = frappe.get_doc("Sales Invoice",name) 
         # doc = frappe.db.get_last_doc("Sales Invoice", filters={"name": name}, order_by="name desc")
-    except Exception as e
+    except Exception as e :
         doc = None
         frappe.throw(_("Doctype Not Found")) 
     return doc
