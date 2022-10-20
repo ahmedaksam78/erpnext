43bf112d679ee2232aa751d722d07f68b8179cf2
ahmedaksam78 <ahmed787878787899@gmail.com>
2022-10-20 14:21:47 +0200

    update hooks

--- erpnext/shipping_and_transportation/doctype/customer_shipment/customer_shipment.js
+++ erpnext/shipping_and_transportation/doctype/customer_shipment/customer_shipment.js
@@ -2,8 +2,15 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Customer Shipment', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		
+		frappe.call({
+			method :'erpnext.api.get_sales_invoice',
+			args :  {name:"ACC-SINV-2022-00001"}   ,
+			callback : function(r){
+				console.log(r);
+			}
+		})
+	}
 	
 });
